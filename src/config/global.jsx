adfcc0dc249a476dd4e70cb4dff3712fa9f0a828
sirajkhan <sirajkhank819@gmail.com>
2026-09-11 17:28:31 +0500

    fix:global

--- src/config/global.jsx
+++ src/config/global.jsx
@@ -4,4 +4,4 @@ window.isValidEmail = email => /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/.test(email)
 
 window.toastify = (msg, type) => console.log(msg, type) // Overridden in App component to use context-aware messages
 
-window.api = "http://localhost:8000"
+window.api = import.meta.env.VITE_API_URL || "http://localhost:8000";
